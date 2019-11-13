8483f15efedaa2ac901a35f426540ab6af9c54d5
Zeynalli Farid <faridaz@code.edu.az>
2019-11-13 11:51:27 -0800

    initial adding

---
+++ GetModelExtension.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    static class GetModelExtension
+    {
+        public static TResultModel getModel<TDbModel, TResultModel>(this TDbModel dbModel) where TResultModel : new()
+        {
+            var before = GC.GetTotalMemory(false);
+
+            var dbModelProperties = dbModel.GetType().GetProperties();
+
+            var resultModel = new TResultModel();
+            var resultModelProperties = resultModel.GetType().GetProperties();
+
+            foreach (var prop in resultModelProperties)
+            {
+                prop.SetValue(resultModel, dbModel.GetType().GetProperty(prop.Name).GetValue(dbModel));
+            }
+
+            //// yuxaridakinda her defe gettype deyib ordan getProperty ile propertini gotururem, 
+            //// yeni reflection her defe isleyir (ya da men ele fikirlesirem)
+            //// ona gore de daha cox memory consume eleyeceyini fikirlesirdim
+            //// amma eksini gordum, yeni asagidaki ondan cox memory isledir
+            //// "dbModelProperties" ozum qesden yuxarida yazdim ve gordum ki 
+            //// ferq mehz ona goredi, yeni foreach-lerin icindekiler ferqe tesir elemir
+            //// ona gore de oxunaqli olsun deye yuxaridaki "foreach" saxladim
+
+
+            //foreach (var prop in resultModelProperties)
+            //{
+            //    foreach (var item in dbModelProperties)
+            //    {
+            //        if (item.Name.Equals(prop.Name))
+            //        {
+            //            prop.SetValue(resultModel, item.GetValue(dbModel));
+            //        }
+            //    }
+            //}
+            var after = GC.GetTotalMemory(false);
+            Console.WriteLine("before -> " + before);
+            Console.WriteLine("after -> " + after);
+            Console.WriteLine("difference without GC and finalizer is " + (after - before));
+
+            return resultModel;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nationality = new Nationality { Id = 2, Name = "Azerbaijanian" };
+            var dbEntity = new AzeriPersonDb
+            {
+                Id = 1,
+                Name = "Farid",
+                Surname = "Zeynalli",
+                Age = 36,
+                Nationality = nationality,
+                Email = "admin@admin.com"
+            };
+
+            var model = dbEntity.getModel<AzeriPersonDb, AzeriPersonViewModel>();
+
+            // breakpoint qoymadan methodun islediyinden emin olmaq ucun bunu yazdiriram 
+            var resultString = $"{model.Name} {model.Surname} is {model.Age} years old. " +
+                               $"He is {model.Nationality.Name}. ";
+
+            Console.WriteLine(resultString);
+
+            Console.ReadLine();
+
+        }
+        
+    }
+
+    public class EmployeeDb
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public Department Department { get; set; }
+
+    }
+    public class Department
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public Department Department { get; set; }
+    }
+
+    public class PersonDB
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public Nationality Nationality { get; set; }
+    }
+    public class AzeriPersonDb : PersonDB
+    {
+        public string Surname { get; set; }
+    }
+    public class Nationality
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class AzeriPersonViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public Nationality Nationality { get; set; }
+    }
+}
